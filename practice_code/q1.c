b60add59bef155dd1bee1050902704f63f93c7a4
sinchan <sinchandas777@gmail.com>
2025-07-27 21:31:03 +0530

    submit

--- practice_code/q1.c
+++ practice_code/q1.c
@@ -1,4 +1,28 @@
-#include<stdio.h>
-int main(){
-    printf("hello world")
+#include <stdio.h>
+
+void insertElement(int arr[], int n, int x, int pos) {
+    
+    for (int i = n - 1; i >= pos - 1; i--) {
+        arr[i + 1] = arr[i];
+    }
+
+    
+    arr[pos - 1] = x;
+
+    // Print updated array
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[100] = {10, 20, 30, 40, 50};  // Pre-allocated size
+    int n = 5;
+    int x = 25;
+    int pos = 3;
+
+    insertElement(arr, n, x, pos);
+
+    return 0;
 }
